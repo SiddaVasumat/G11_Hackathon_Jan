fb03ea0a450f85cfa06974dab21d2d07a098d0dc
SiddaVasumat <vasumat.sidda@gmail.com>
2026-01-22 22:47:27 +0530

    Editing Bugs

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,14 +1,12 @@
 import { supabase } from './supabase'
-
 export const signInWithEmail = async (email: string) => {
   const { error } = await supabase.auth.signInWithOtp({
     email,
+    options: {
+      emailRedirectTo: window.location.origin,
+    },
   })
 
   return { error }
 }
 
-export const signOut = async () => {
-  const { error } = await supabase.auth.signOut()
-  return { error }
-}
